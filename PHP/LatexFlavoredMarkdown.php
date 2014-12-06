014af5ff7211ebf339734524f5d6be4f647a6c65
TheMrNomis <n0m1s@homnomnom.fr>
2014-12-06 15:10:00 +0100

    added function replacement

--- PHP/LatexFlavoredMarkdown.php
+++ PHP/LatexFlavoredMarkdown.php
@@ -5,6 +5,7 @@
     $origin_text = file_get_contents("../example.lmd");
     $text = explode("$$", $origin_text);
     print_r($text[1]);
+    echo("\n+----------------------------------------+\n");
     for($i=1; $i<count($text); $i+=2)
     {
         $text[$i] = Math::transform($text[$i]);
--- PHP/mathHandler.php
+++ PHP/mathHandler.php
@@ -1,9 +1,47 @@
 <?php
+    function getBooleanAt($haystack, $needle)
+    {
+        return ($haystack / pow(2,$needle)) % 2;
+    }
+
     class Math
     {
+        private static function tranformFunctions($subject)
+        {
+            $lines = file("functionsReplacementList", FILE_IGNORE_NEW_LINES);
+            foreach($lines as $line)
+            {
+                $line = str_replace(" ","",$line);
+                $line = str_replace("*","\*",$line);
+                if($line[0] != "#")
+                {
+                    $subject = str_replace("\n", " \n", $subject); //line end gestion (add spaces before \n)
+
+                    $strippedLine = explode("#",$line); //comment gestion
+                    list($nb, $LFM, $latex) = explode("@", $strippedLine[0]);
+
+                    $nb = intval($nb);
+                    for($i = 0; $i < pow(2,$nb); $i++)
+                    {
+                        $LFM_tmp = $LFM;
+                        for($j = 1; $j <= $nb; $j++)
+                        {
+                            if(getBooleanAt($i,$j-1) == 0)
+                                $var = "\s{0,}\{(.+)\}\s{0,}";
+                            else
+                                $var = "\s{1,}(\S+)\s{1,}";
+                            $LFM_tmp = str_replace("$".$j."", $var, $LFM_tmp);
+                        }
+                        $subject = preg_replace("#".$LFM_tmp."#U", " ".$latex." ", $subject);
+                    }
+                }
+            }
+
+            return $subject;
+        }
+
         private static function transformSymbols($subject)
         {
-            //remplacement
             $lines = file("symbolsReplacementList", FILE_IGNORE_NEW_LINES);
             foreach($lines as $line)
             {
@@ -27,7 +65,7 @@
                         break;
                     }
                     $latex = " ".$latex." ";
-                    $subject = str_ireplace($LFM, $latex, $subject);
+                    $subject = str_replace($LFM, $latex, $subject);
                 }
             }
 
@@ -36,7 +74,9 @@
 
         public static function transform($subject)
         {
+            $subject = preg_replace("#\s(\S{1,})\*(\S{1,})\s#U", "{ $1 \\cdot $2 }", $subject);
             $subject = Math::transformSymbols($subject);
+            $subject = Math::tranformFunctions($subject);
             return $subject;
         }
     }
